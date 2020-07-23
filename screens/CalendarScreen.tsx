ec55d9a9837ca50ecdef404bf903866d4df21d1c
Willane Paiva <willane@vizir.com.br>
2020-07-23 15:28:04 -0300

    feat: initial screens

---
+++ screens/CalendarScreen.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {DrawerToggle, headerOptions} from '../menus/HeaderComponents';
+
+const Stack = createStackNavigator();
+
+// Temporary placeholder view
+const CalendarComponent = () => (
+  <View style={styles.container}>
+    <Text>Calendar</Text>
+  </View>
+);
+
+export default class CalendarScreen extends React.Component {
+  render() {
+    return (
+      <Stack.Navigator screenOptions={headerOptions}>
+        <Stack.Screen
+          name="Calendar"
+          component={CalendarComponent}
+          options={{
+            title: 'Calendar',
+            headerLeft: () => <DrawerToggle />,
+          }}
+        />
+      </Stack.Navigator>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {ActivityIndicator, Alert, StyleSheet, Text, View} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {DrawerToggle, headerOptions} from '../menus/HeaderComponents';
+
+const Stack = createStackNavigator();
+const UserState = React.createContext({userLoading: true, userName: ''});
+
+type HomeScreenState = {
+  userLoading: boolean;
+  userName: string;
+};
+
+const HomeComponent = () => {
+  const userState = React.useContext(UserState);
+
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator animating={userState.userLoading} size="large" />
+      {userState.userLoading ? null : <Text>Hello {userState.userName}!</Text>}
+    </View>
+  );
+};
+
+export default class HomeScreen extends React.Component {
+  state: HomeScreenState = {
+    userLoading: true,
+    userName: '',
+  };
+
+  render() {
+    return (
+      <UserState.Provider value={this.state}>
+        <Stack.Navigator screenOptions={headerOptions}>
+          <Stack.Screen
+            name="Home"
+            component={HomeComponent}
+            options={{
+              title: 'Welcome',
+              headerLeft: () => <DrawerToggle />,
+            }}
+          />
+        </Stack.Navigator>
+      </UserState.Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
